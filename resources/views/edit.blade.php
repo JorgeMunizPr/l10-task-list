05000f043b9e775d1f9610d560e423d765e4ffee
Jorge Muniz <jorgemp221@msn.com>
2024-02-23 07:47:03 -0600

    Request class for Task Reusable code for validate data Reading from id to task class

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -11,7 +11,7 @@
 
 
 @section('content')
-    <form method="POST" action="{{ route('tasks.update', ['id' => $task->id]) }}">
+    <form method="POST" action="{{ route('tasks.update', ['task' => $task->id]) }}">
         @csrf
         @method('put')
             <div>
